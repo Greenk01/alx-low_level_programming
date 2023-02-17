998b3ff611bc6aab591608b6ea310a24b7aded61
Greenk01 <kimgreen556@gmail.com>
2023-02-17 10:19:47 -0800

    alphabets3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib>
+
+/**
+ * main - Alphabet in both upper and lower case
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch = 'a';
+	int CH = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+
+	putchar('\n');
+	return (0);
+}
