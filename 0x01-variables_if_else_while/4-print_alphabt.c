3e7acc0b2633eab6fa1f673aa01deca32f2619bb
Greenk01 <kimgreen556@gmail.com>
2023-02-17 11:51:06 -0800

    Alphabt4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,8 +2,10 @@
 
 /**
  * main - Prints alphabet in lowercase, followed by a new line
+ *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char c;
@@ -11,7 +13,7 @@ int main(void)
 	c = 'a';
 	while
 		(c <= 'z') {
-			if ((c != 'e' || c != 'q') && c <= 'z')
+			if ((c != 'e' && c != 'q') && c <= 'z')
 				putchar(c);
 			c++;
 		}
