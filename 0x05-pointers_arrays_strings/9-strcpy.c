d77ae27bc323a125d52d655950f3673dbf7d443c
Greenk01 <kimgreen556@gmail.com>
2023-02-27 09:35:19 -0800

    9 strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,6 +12,10 @@ char *_strcpy(char *dest, char *src)
 
 	while (*(src + len) != '\0')
 	{
-		*(dest + len) = '\0';
-		return (dest);
+		*(dest + len) = *(src + len);
+		len++;
+	}
+
+	*(dest + len) = '\0';
+	return (dest);
 }
